344bf465ad31c354b80113df9c3ab465955ef0f7
Davide <885838@stud.unive.it>
2022-12-14 09:43:02 +0100

    Aggiunto javadoc a classe Evento. Creato MacroD e implementato alcuni giochi (PallaPrigioniera, RubaBandiera, TiroAllaFune). Creata classe utility Pair per la gestione delle squadre.

--- app/src/main/java/com/example/viviappis/data/model/Evento.java
+++ app/src/main/java/com/example/viviappis/data/model/Evento.java
@@ -15,7 +15,15 @@ public class Evento
     private Collection<Utente> partecipants;
     private boolean isPublic;
 
-
+    /**
+     * Constructor of object Evento.
+     * @param name nome dell'evento
+     * @param description descrizione dell'evento
+     * @param creator utente creatore dell'evento
+     * @param date data e ora dell'evento
+     * @param password password di accesso all'evento
+     * @param isPublic definisce se l'evento è pubblico oppure privato
+     */
     public Evento (String name, String description, Utente creator, String date, String password, boolean isPublic){
         this.name = name;
         this.creator = creator;
@@ -26,47 +34,108 @@ public class Evento
         this.isPublic = isPublic;
     }
 
+    /**
+     * Return the name of the event.
+     * @return name of the event
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * Return the password oth the event.
+     * @return password of the event
+     */
     public String getPassword() {
         return password;
     }
+
+    /**
+     * Return the time of the event.
+     * @return time of the event
+     */
     public String getDate() {
         return date;
     }
+
+    /**
+     * Return the users that partecipate to the event.
+     * @return partecipants in the event
+     */
     public Collection<Utente> getPartecipants() {
         return partecipants;
     }
+
+    /**
+     * Return the description of the event
+     * @return description of the event
+     */
     public String getDescription() {
         return description;
     }
+
+    /**
+     * Return the users the created the event.
+     * @return creator of the event
+     */
     public Utente getCreator() {
         return creator;
     }
 
+    /**
+     * set the date of the instance to the value of the param date.
+     * @param date time to set as time of the event
+     */
     public void setDate(String date) {
         this.date = date;
     }
+
+    /**
+     * set the description of the instance to the value of the param description.
+     * @param description to set as description of the event
+     */
     public void setDescription(String description) {
         this.description = description;
     }
+
+    /**
+     * set the name of the instance to the value of the param name.
+     * @param name to set as name of the event
+     */
     public void setName(String name) {
         this.name = name;
     }
-    public void setPublic(boolean aPublic) {
-        isPublic = aPublic;
+
+    /**
+     * set the isPublic of the instance to the value of the param isPublic.
+     * @param isPublic value to set defining if the event is public or not
+     */
+    public void setPublic(boolean isPublic) {
+        this.isPublic = isPublic;
     }
 
+    /**
+     * add the user u to the partecipants list.
+     * @param u user to add to partecipants list
+     */
     public void addPartecipants(Utente u) {
         this.partecipants.add(u);
     }
 
+    /**
+     * Return if the event is public or not.
+     * @return if the event is public or not
+     */
     public boolean isPublic() {
         return isPublic;
     }
 
-
+    /**
+     * Create a Map of an event using Strings as keys and the fields of the event as values.
+     * We use it in order to communicate the values of the event to the database.
+     * @param e the event to push in the database
+     * @return return a map having fields' name as keys and event fields' values as value
+     */
     public static Map<String, Object> eventMap(Evento e){
         Map<String, Object> event = new HashMap<>();
         event.put("name", e.getName());
--- app/src/main/java/com/example/viviappis/data/model/MacroAree/AreaD/MacroD.java
+++ app/src/main/java/com/example/viviappis/data/model/MacroAree/AreaD/MacroD.java
@@ -1,10 +1,183 @@
 package com.example.viviappis.data.model.MacroAree.AreaD;
 
+import com.example.viviappis.Pair;
+import com.example.viviappis.data.model.Evento;
 import com.example.viviappis.data.model.GenericEvent;
 import com.example.viviappis.data.model.Utente;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MacroD extends GenericEvent {
-    public MacroD(String name, String description, Utente creator, String date, String password, boolean isPublic, int minPart, int maxPart) {
+    private Pair<String, Collection<Utente>> squadra1;
+    private Pair<String, Collection<Utente>> squadra2;
+    private Map<Utente, Integer> ranklist;
+    private final int points;
+    private Pair<String, Collection<Utente>> winners;
+
+    /**
+     * Constructor of object MacroD
+     * @param name name of the event
+     * @param description description of the event
+     * @param creator user creator of the event
+     * @param date time of the event
+     * @param password password to log into the event
+     * @param isPublic define if the event is public or not
+     * @param minPart define the minimum number of partecipants
+     * @param maxPart define the maximum number of partecipants
+     * @param nameSquadra1 name of the team 1
+     * @param nameSquadra2 name of the team 2
+     * @param points points gained for the victory
+     */
+    public MacroD(String name, String description, Utente creator, String date, String password, boolean isPublic, int minPart, int maxPart, String nameSquadra1, String nameSquadra2, int points) {
         super(name, description, creator, date, password, isPublic, minPart, maxPart);
+        this.points = points;
+        this.squadra1 = new Pair<>(nameSquadra1, new ArrayList<>());
+        this.squadra2 = new Pair<>(nameSquadra2, new ArrayList<>());
+        this.ranklist = new HashMap<>();
+        this.winners = new Pair<>("", new ArrayList<>());
     }
+
+    /**
+     * Add one user to team 1
+     * @param u
+     */
+    public void addToSquadra1(Utente u){
+        this.squadra1.getY().add(u);
+    }
+
+    /**
+     * Add one user to team 2
+     * @param u
+     */
+    public void addToSquadra2(Utente u){
+        this.squadra2.getY().add(u);
+    }
+
+    /**
+     * Return the name of team 1
+     * @return
+     */
+    public String getNameSquadra1(){
+        return this.squadra1.getX();
+    }
+
+    /**
+     * Return a collection of users that belong to team 1
+     * @return
+     */
+    public Collection<Utente> getSquadra1(){
+        return this.squadra1.getY();
+    }
+
+    /**
+     * Return the name of team 2
+     * @return
+     */
+    public String getNameSquadra2(){
+        return this.squadra2.getX();
+    }
+
+    /**
+     * Set team 1's name
+     * @param nameSquadra1
+     */
+    public void setNameSquadra1(String nameSquadra1){
+        this.squadra1.setX(nameSquadra1);
+    }
+
+    /**
+     * Set team 2's name
+     * @param nameSquadra2
+     */
+    public void setNameSquadra2(String nameSquadra2){
+        this.squadra2.setX(nameSquadra2);
+    }
+
+    /**
+     * Return a collection of users that belong to team 1
+     * @return
+     */
+    public Collection<Utente> getSquadra2(){
+        return this.squadra2.getY();
+    }
+
+    /**
+     * Return the ranklist of the event as a map holding users' names as keys and relatives points as values
+     * @return
+     */
+    public Map<Utente, Integer> getRanklist() {
+        return ranklist;
+    }
+
+    /**
+     * Give points to all users in winner team
+     */
+    public void addPoints(){
+        for(Utente u: this.winners.getY()){
+            u.setScore(u.getScore() + this.points);
+        }
+    }
+
+    /**
+     * Create the event's ranklist sorting for descending values
+     */
+    public void generateRanklistInOrder(){
+        for(Utente u: this.getPartecipants()){
+            this.ranklist.put(u, u.getScore());
+        }
+        List<Map.Entry<Utente, Integer>> list = new ArrayList<>(this.ranklist.entrySet());
+        list.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        this.ranklist.clear();
+        for(Map.Entry<Utente, Integer> u: list){
+            this.ranklist.put(u.getKey(), u.getValue());
+        }
+        //TODO: da rivere perchè probabilmente è ordinata in ordine crescente invece che decrescente KEKW. Meme del dev che piange per la sua incompetenza :')
+        //TODO UPDATE: ok forse è risolto ma lo scopriremo a tempo debito
+    }
+
+    /**
+     * Add winner users to winners map field
+     * @param winners
+     */
+    public void setWinners(Pair<String, Collection<Utente>> winners){
+        this.winners.setX(winners.getX());
+        for(Utente u: winners.getY()) {
+            this.winners.getY().add(u);
+        }
+    }
+
+    /**
+     * Return the winner team
+     * @return
+     */
+    public Pair<String, Collection<Utente>> getWinners(){
+        return this.winners;
+    }
+
+    /**
+     * Create a Map of an event using Strings as keys and the fields of the event as values.
+     * We use it in order to communicate the values of the event to the database.
+     * @return return a map having fields' name as keys and event fields' values as value
+     */
+    public Map<String, Object> macroDEventMap(){
+        Map<String, Object> mdeMap = new HashMap<>();
+        mdeMap.put("name", this.getName());
+        mdeMap.put("description", this.getDescription());
+        mdeMap.put("date", this.getDate());
+        mdeMap.put("creator", this.getCreator());
+        mdeMap.put("password", this.getPassword());
+        mdeMap.put("partecipants", this.getPartecipants());
+        mdeMap.put("public", this.isPublic());
+        mdeMap.put("team 1", this.getSquadra1());
+        mdeMap.put("team 2", this.getSquadra2());
+        mdeMap.put("winners", this.getWinners());
+        mdeMap.put("ranklist", this.getRanklist());
+        return mdeMap;
+    }
+
 }
