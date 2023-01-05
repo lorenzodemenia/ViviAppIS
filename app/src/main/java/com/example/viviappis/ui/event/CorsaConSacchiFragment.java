fa84d668cf7c46d52de568328778547af356ce3c
Davide885838 <885838@stud.unive.it>
2023-01-05 19:15:39 +0100

    Apportate alcune modifiche ai costruttori dei giochi in MacroA. Aggiunte le descrizioni di tutti i giochi nel file strings.xml. Creata EventInExecutionActivity e implementata selezione gioco in base al tipo di evento. Creati fragment per ogni gioco.

---
+++ app/src/main/java/com/example/viviappis/ui/event/CorsaConSacchiFragment.java
@@ -0,0 +1,7 @@
+package com.example.viviappis.ui.event;
+
+import androidx.fragment.app.Fragment;
+
+
+public class CorsaConSacchiFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/viviappis/ui/event/NascondinoFragment.java
@@ -0,0 +1,6 @@
+package com.example.viviappis.ui.event;
+
+import androidx.fragment.app.Fragment;
+
+public class NascondinoFragment extends Fragment {
+}
