ca0bce422f147532f836792827555b878ad4a898
Davide885838 <885838@stud.unive.it>
2023-01-13 10:46:13 +0100

    impostato setting layout eventi in esecuzione

---
+++ app/src/main/java/com/example/viviappis/ui/event/EventInExecutionFragment.java
@@ -0,0 +1,10 @@
+package com.example.viviappis.ui.event;
+
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
+
+public class EventInExecutionFragment extends Fragment {
+    private Spinner spinFirst, spinSec, spinThird;
+
+}
