3dfd7e5549c7aecfd43a95c3a58823a5e2152622
Lorenzo Demenia <71456537+lorenzodemenia@users.noreply.github.com>
2023-01-13 14:40:41 +0100

    sistemato un pò la grafica di alcune cose

--- app/src/main/java/com/example/viviappis/MainActivity.java
+++ app/src/main/java/com/example/viviappis/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.viviappis;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,6 +38,8 @@ public class MainActivity extends AppCompatActivity
         {
             startActivity(new Intent(this, AfterLogin.class));
         }
+
+
     }
 }
 
