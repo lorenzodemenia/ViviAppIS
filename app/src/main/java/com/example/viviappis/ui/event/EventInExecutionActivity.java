ccc11ca27aa4bac123219c1cb653f68240274c19
Davide885838 <885838@stud.unive.it>
2023-01-14 11:57:52 +0100

    Testati e  ultimati eventi in esecuzione a squadre. Funzionano. Godo.

--- app/src/main/java/com/example/viviappis/ui/event/EventInExecutionActivity.java
+++ app/src/main/java/com/example/viviappis/ui/event/EventInExecutionActivity.java
@@ -1,14 +1,17 @@
 package com.example.viviappis.ui.event;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -44,18 +47,18 @@ public class EventInExecutionActivity extends AppCompatActivity {
     private String eventTypeString;
 
     private int nRoundTeam = 1;
-    Button endGameTeam = (Button) findViewById(R.id.endGameTeam);
-    Button replayTeam = (Button) findViewById(R.id.replayTeam);
-    Button team1Winner = (Button) findViewById(R.id.team1Winner);
-    Button team2Winner = (Button) findViewById(R.id.team2Winner);
-    TextView titleTeam = (TextView) findViewById(R.id.titleTeam);
-    TextView team1players = (TextView) findViewById(R.id.team1players);
-    TextView team2players = (TextView) findViewById(R.id.team2players);
-    TextView roundTeam = (TextView) findViewById(R.id.roundTeam);
-    Spinner spinTeam1 = (Spinner) findViewById(R.id.spinTeam1);
-    Spinner spinTeam2 = (Spinner) findViewById(R.id.spinTeam2);
-    TextView team1txt = (TextView) findViewById(R.id.team1);
-    TextView team2txt = (TextView) findViewById(R.id.team2);
+    Button endGameTeam;
+    Button replayTeam;
+    Button team1Winner;
+    Button team2Winner;
+    TextView titleTeam;
+    TextView team1players;
+    TextView team2players;
+    TextView roundTeam;
+    Spinner spinTeam1;
+    Spinner spinTeam2;
+    TextView team1txt;
+    TextView team2txt;
     Map<String, Integer> tempPointsTeam = new HashMap<>();
     boolean t1Wins = false;
     boolean t2Wins = false;
@@ -69,6 +72,7 @@ public class EventInExecutionActivity extends AppCompatActivity {
 
         id = getIntent().getStringExtra(getResources().getString(R.string.event_send_ev));
 
+
         db.collection(getResources().getString(R.string.db_rac_events)).document(id).get().addOnCompleteListener((t)->
         {
             if (t.isSuccessful())
@@ -78,39 +82,60 @@ public class EventInExecutionActivity extends AppCompatActivity {
                 switchLayout();
             }
         });
-        if(eventTypeString.equals("team")){
+
+    }
+
+    private void switchLayout()
+    {
+        String[] s = getResources().getStringArray(R.array.new_ev_spinner_item);
+
+        if(eventType.equals(s[0]) || eventType.equals(s[1]) || eventType.equals(s[2])){
+            setContentView(R.layout.activity_event_in_execution_team_game);
+            endGameTeam = (Button) findViewById(R.id.endGameTeam);
+            replayTeam = (Button) findViewById(R.id.replayTeam);
+            team1Winner = (Button) findViewById(R.id.team1Winner);
+            team2Winner = (Button) findViewById(R.id.team2Winner);
+            titleTeam = (TextView) findViewById(R.id.titleTeam);
+            team1players = (TextView) findViewById(R.id.team1players);
+            team2players = (TextView) findViewById(R.id.team2players);
+            roundTeam = (TextView) findViewById(R.id.roundTeam);
+            spinTeam1 = (Spinner) findViewById(R.id.spinTeam1);
+            spinTeam2 = (Spinner) findViewById(R.id.spinTeam2);
+            team1txt = (TextView) findViewById(R.id.team1);
+            team2txt = (TextView) findViewById(R.id.team2);
             titleTeam.setText(eventType);
-            roundTeam.setText("Round " + Integer.toString(nRoundTeam));
-            team1txt.setText("Squadra 1");
-            team2txt.setText("Squadra 2");
-            endGameTeam.setText("Termina Evento");
-            replayTeam.setText("Rigioca");
-            team1Winner.setText("Vince Squadra 1");
-            team2Winner.setText("Vince Squadra 2");
+            team1players.setMovementMethod(new ScrollingMovementMethod());
+            team2players.setMovementMethod(new ScrollingMovementMethod());
+            roundTeam.setText(getResources().getString(R.string.ev_in_exec_round) + " " + Integer.toString(nRoundTeam));
+            team1txt.setText(getResources().getString(R.string.ev_in_exec_team1));
+            team2txt.setText(getResources().getString(R.string.ev_in_exec_team1));
+            endGameTeam.setText(getResources().getString(R.string.ev_in_exec_endgame));
+            replayTeam.setText(getResources().getString(R.string.ev_in_exec_replay));
+            team1Winner.setText(getResources().getString(R.string.ev_in_exec_team1_wins));
+            team2Winner.setText(getResources().getString(R.string.ev_in_exec_team2_wins));
             addPartecipantsToMap();
             createPartString();
-            setSpinner(spinTeam1, team1, team1players);
-            setSpinner(spinTeam2, team2, team2players);
+            setSpinner(spinTeam1, team1, team2, team1players, team2players);
+            setSpinner(spinTeam2, team2, team1, team2players, team1players);
             addActionListenerTeam();
-        }else if(eventTypeString.equals("single")){
-
+        }else{
+            eventTypeString = "single";
+            setContentView(R.layout.activity_event_in_execution_single_game);
         }
     }
 
     private void createPartString(){
-        List<String> tmp = new ArrayList<>(Arrays.asList(part));
+        List<String> tmp = new ArrayList<>();
+        tmp.add("Aggiungi Giocatore");
         tmp.addAll(e.getPartecipants());
-        part = tmp.toArray(part);
+        part = tmp.toArray(new String[0]);
     }
 
     private void printListPlayer(TextView tv, List<String> team){
-        String players = team.get(0);
-        String tmp = players;
-        team.remove(players);
+        String players = "";
         for(String s: team){
             players += "\n" + s + " " + tempPointsTeam.get(s).toString();
         }
-        team.add(tmp);
         tv.setText(players);
     }
 
@@ -133,7 +158,7 @@ public class EventInExecutionActivity extends AppCompatActivity {
         });
     }
 
-    private void setSpinner(Spinner spinner, List<String> team, TextView tv){
+    private void setSpinner(Spinner spinner, List<String> teamSelected, List<String> otherTeam, TextView tvSelected, TextView otherTV){
         ArrayAdapter<String> adapter = new ArrayAdapter<>(EventInExecutionActivity.this, android.R.layout.simple_spinner_item, part);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -142,11 +167,19 @@ public class EventInExecutionActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
-                team.add(item);
-                List<String> l = new ArrayList<>(Arrays.asList(part));
-                l.remove(item);
-                part = l.toArray(part);
-                printListPlayer(tv, team);
+                if(e.getPartecipants().contains(item)){
+                    if(otherTeam.contains(item)){
+                        otherTeam.remove(item);
+                    }
+                    teamSelected.add(item);
+                    printListPlayer(tvSelected, teamSelected);
+                    printListPlayer(otherTV, otherTeam);
+                }
+
+            }
+
+            private void setText(){
+
             }
 
             @Override
@@ -186,8 +219,11 @@ public class EventInExecutionActivity extends AppCompatActivity {
             t2Wins = false;
         }
         nRoundTeam++;
-        roundTeam.setText("Round " + Integer.toString(nRoundTeam));
-
+        roundTeam.setText(getResources().getString(R.string.ev_in_exec_round) + " " + Integer.toString(nRoundTeam));
+        printListPlayer(team1players, team1);
+        printListPlayer(team2players, team2);
+        team1Winner.setBackgroundColor(getResources().getColor(R.color.giallocra));
+        team2Winner.setBackgroundColor(getResources().getColor(R.color.giallocra));
     }
 
     private void addPartecipantsToMap(){
@@ -197,18 +233,7 @@ public class EventInExecutionActivity extends AppCompatActivity {
     }
 
     //passare almeno id all pagina
-    private void switchLayout()
-    {
-        String[] s = getResources().getStringArray(R.array.new_ev_spinner_item);
 
-        if(eventType.equals(s[0]) || eventType.equals(s[1]) || eventType.equals(s[2])){
-            eventTypeString = "team";
-            setContentView(R.layout.activity_event_in_execution_team_game);
-        }else{
-            eventTypeString = "single";
-            setContentView(R.layout.activity_event_in_execution_single_game);
-        }
-    }
 
     public Map<String, Object> filterListByID(List<DocumentSnapshot> l, String id)
     {
