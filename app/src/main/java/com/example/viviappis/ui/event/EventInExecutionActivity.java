f0bfed07151d33475c07b90e7e6f8fb22e6757ce
Agostino Calabrò <974519@stud.unive.it>
2023-01-15 19:41:09 +0100

    gioco singolo terminato

--- app/src/main/java/com/example/viviappis/ui/event/EventInExecutionActivity.java
+++ app/src/main/java/com/example/viviappis/ui/event/EventInExecutionActivity.java
@@ -1,7 +1,5 @@
 package com.example.viviappis.ui.event;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -11,13 +9,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.viviappis.Pair;
 import com.example.viviappis.R;
 import com.example.viviappis.control.AfterLogin;
 import com.example.viviappis.data.model.Evento;
@@ -29,9 +24,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -149,9 +142,9 @@ public class EventInExecutionActivity extends AppCompatActivity {
             fineEventoButton = (Button) findViewById(R.id.button_fine_evento);
             staSotto = (TextView) findViewById(R.id.textViewStaSotto);
             scegli = (TextView) findViewById(R.id.textViewScegli);
-            testoPrimo = (TextView) findViewById(R.id.textViewTestPrimo);
-            testoSecondo = (TextView) findViewById(R.id.textViewTestSecondo);
-            testoTerzo = (TextView) findViewById(R.id.textViewTestTerzo);
+            testoPrimo = (TextView) findViewById(R.id.textView1c);
+            testoSecondo = (TextView) findViewById(R.id.textView2c);
+            testoTerzo = (TextView) findViewById(R.id.textView3c);
             contienePrimo = (TextView) findViewById(R.id.textViewPrimo);
             contieneSecondo = (TextView) findViewById(R.id.textViewSecondo);
             contieneTerzo = (TextView) findViewById(R.id.textViewTerzo);
